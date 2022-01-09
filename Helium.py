0d3fed833530dd336b1145b5f3064e04e26a56e2
Milo Oien-Rochat <nikolarobottesla@github.com>
2022-01-09 10:06:02 -0600

    added functionality to get and export each hotspot's earnings individually

--- Helium.py
+++ Helium.py
@@ -6,6 +6,37 @@ import requests
 from datetime import date, timedelta
 
 
+def export_wallet_taxes_by_hotspot(account_address, year=2021):
+    # your HNT address
+    print('Collecting earnings data for account address: ', account_address)
+
+    url = 'https://api.helium.io/v1/accounts/' + account_address + '/hotspots'
+    hotspot_data = request_json(url)
+
+    hotspot_added_dates = []
+    for i in range(len(hotspot_data['data'])):
+        date_added_iso = hotspot_data['data'][i]['timestamp_added'].split('T')[0]
+        date_added_ts = date.fromisoformat(date_added_iso)
+        hotspot_added_dates.append(date_added_ts)
+
+    hnt_price_usd = pd.read_csv(f'coingecko_prices_{year}.csv')['HNT_PRICE_USD']
+    total_taxable_value_usd = 0
+    hotspots = {}
+    dfs = {}
+    for i in range(len(hotspot_data['data'])):
+        hotspot_address = hotspot_data['data'][i]['address']
+        hotspot_name = hotspot_data['data'][i]['name']
+        hotspot = MinedByDay([hotspot_address], year, hotspot_added_dates[i])
+        hotspots[hotspot_name] = hotspot
+        hotspot.get_data()
+        dfs[hotspot_name] = hotspot.calc_value(hnt_price_usd)
+        hotspot.export_to_csv(name=hotspot_name)
+        total_taxable_value_usd += np.sum(hotspot.value_mined_usd)
+
+    print(f'\n\nTOTAL TAXABLE INCOME (USD) FOR {year}: ', str(total_taxable_value_usd))
+    return dfs, total_taxable_value_usd
+
+
 def export_wallet_taxes(account_address, year=2021):
     # your HNT address
     print('Collecting earnings data for account address: ', account_address)
@@ -24,7 +55,8 @@ def export_wallet_taxes(account_address, year=2021):
     for i in range(len(hotspot_data['data'])):
         hotspot_addresses.append(hotspot_data['data'][i]['address'])
 
-    day_list_iso, mined_by_day = get_mined_by_day(hotspot_addresses, year, account_start_date, hotspot_added_dates)
+    hotspots = MinedByDay(hotspot_addresses, year, account_start_date)
+    day_list_iso, mined_by_day = hotspots.get_data()
 
     hnt_price_usd = pd.read_csv(f'coingecko_prices_{year}.csv')['HNT_PRICE_USD']
     value_mined_usd = np.array(hnt_price_usd[:len(mined_by_day)]) * np.array(mined_by_day)
@@ -51,50 +83,73 @@ def export_hotspot_taxes(hotspot_address, year=2021):
     hotspot_address = hotspot_data['data']['address']
     hotspot_name = hotspot_data['data']['name']
 
-    day_list_iso, mined_by_day = get_mined_by_day([hotspot_address], year, hotspot_start_date)
-
-    hnt_price_usd = pd.read_csv(f'coingecko_prices_{year}.csv')['HNT_PRICE_USD']
-    value_mined_usd = np.array(hnt_price_usd[:len(mined_by_day)]) * np.array(mined_by_day)
-    df = pd.DataFrame(data=[day_list_iso, mined_by_day, hnt_price_usd[:len(mined_by_day)], value_mined_usd]).transpose()
-    df.columns = ['DATE', 'HNT_MINED', 'HNT_PRICE_USD', 'VALUE_MINED_USD']
-    # df.to_csv(EXPORT_PATH)
+    hotspot = MinedByDay([hotspot_address], year, hotspot_start_date)
+    hotspot.get_data()
+    df = hotspot.calc_value()
 
     # summary
-    total_taxable_value_usd = np.sum(value_mined_usd)
+    total_taxable_value_usd = np.sum(hotspot.value_mined_usd)
 
     print(f'\n\nTOTAL TAXABLE INCOME (USD) FOR {year}: ', str(total_taxable_value_usd))
     return df, total_taxable_value_usd, hotspot_name
 
 
-def get_mined_by_day(hotspot_addresses, year, start_date, hotspot_added_dates=[]):
-    year_start_date = date.fromisoformat(f'{year}-01-01')
-    start_date = max(year_start_date, start_date)
-    end_day = date.fromisoformat(f'{year + 1}-01-01')  # need the 1st day of new year in the day list to get every day
-    n_days = end_day - start_date
-    day_list_iso = []
-    for i in range(n_days.days + 1):
-        day = end_day - timedelta(days=i)
-        iso = day.isoformat()
-        day_list_iso.append(iso)
+class MinedByDay:
+    def __init__(self, hotspot_addresses, year, start_date):
+        self.hotspot_addresses = hotspot_addresses
+        self.year = year
+        self.start_date = start_date
+        self.mined_by_day = []
+        self.day_list_iso = []
+        self.value_mined_usd = None
+        self.df = None
 
-    mined_by_day = []
-    for j in range(len(day_list_iso) - 1):
-        daily_total = 0
-        for hotspot in hotspot_addresses:
-            url = "https://api.helium.io/v1/hotspots/" + hotspot +\
-                  "/rewards/sum?max_time=" + day_list_iso[j] + "&min_time=" +\
-                  day_list_iso[j + 1]
-            total = request_json(url)
-            daily_total += total['data']['total']
+    def get_data(self):
+        year_start_date = date.fromisoformat(f'{self.year}-01-01')
+        start_date = max(year_start_date, self.start_date)
+        # need the 1st day of new year in the day list to get every day
+        end_day = date.fromisoformat(f'{self.year + 1}-01-01')
+        n_days = end_day - start_date
+        day_list_iso = self.day_list_iso
+        for i in range(n_days.days + 1):
+            day = end_day - timedelta(days=i)
+            iso = day.isoformat()
+            day_list_iso.append(iso)
 
-        mined_by_day.append(daily_total)
+        mined_by_day = self.mined_by_day
+        for j in range(len(day_list_iso) - 1):
+            daily_total = 0
+            for hotspot in self.hotspot_addresses:
+                url = "https://api.helium.io/v1/hotspots/" + hotspot +\
+                      "/rewards/sum?max_time=" + day_list_iso[j] + "&min_time=" +\
+                      day_list_iso[j + 1]
+                total = request_json(url)
+                daily_total += total['data']['total']
 
-        if np.remainder(j, 10) == 0:
-            print(day_list_iso[j + 1])
+            mined_by_day.append(daily_total)
 
-    del day_list_iso[0]
+            if np.remainder(j, 10) == 0:
+                print(day_list_iso[j + 1])
 
-    return day_list_iso, mined_by_day
+        del day_list_iso[0]
+
+        return day_list_iso, mined_by_day
+
+    def calc_value(self, hnt_price_usd):
+        mined_by_day = self.mined_by_day
+        day_list_iso = self.day_list_iso
+        self.value_mined_usd = np.array(hnt_price_usd[:len(mined_by_day)]) * np.array(mined_by_day)
+        df = pd.DataFrame(
+            data=[day_list_iso, mined_by_day, hnt_price_usd[:len(mined_by_day)], self.value_mined_usd]).transpose()
+        df.columns = ['DATE', 'HNT_MINED', 'HNT_PRICE_USD', 'VALUE_MINED_USD']
+        self.df = df
+        return df
+
+    def export_to_csv(self, export_path='', name=''):
+        if not export_path:
+            export_path = f'{self.year}_{name}.csv'
+
+        self.df.to_csv(export_path)
 
 
 def request_json(url, headers={}, payload={}):
--- examples.py
+++ examples.py
@@ -1,26 +1,31 @@
-from Helium import export_wallet_taxes, export_hotspot_taxes
+from Helium import export_wallet_taxes, export_wallet_taxes_by_hotspot, export_hotspot_taxes
 
-# the functions in Helium.py give you two options for exporting earnings data:
-#   1) export_wallet_taxes(account_address) scrapes the data for all hotspots attached to your HNT wallet
-#   2) export_hotspot_taxes(hotspot_address) scrapes data for an individual hotspot
-# both functions return the following outputs:
+# the functions in Helium.py give you three options for exporting earnings data:
+#   1) export_wallet_taxes scrapes the data for all hotspots attached to your HNT wallet
+#   2) export_wallet_taxes_by_hotspot saves the hotspot earnings individually based on name
+#   3) export_hotspot_taxes scrapes data for an individual hotspot
+# All functions return the following outputs:
 #   - a pandas dataframe containing columns for (DATE, HNT_MINED, HNT_PRICE_USD, VALUE_MINED_USD)
 #   - the total taxable earnings for 2020 for that wallet/hotspot (i.e. the sum of VALUE_MINED_USD)
 #   - (for export_hotspot_taxes ONLY) the name of the hotspot (e.g. energetic-brown-bear)
-# to export a CSV, use the built-in pandas method: df.to_csv(EXPORT_PATH)
+# to export a CSV with methods 1 or 3 use the built-in pandas method: df.to_csv(EXPORT_PATH)
 # *** DISCLAIMER: THIS IS NOT OFFICIAL TAX ADVICE AND SHOULD ONLY BE USED FOR EDUCATIONAL/EXPLORATORY PURPOSES ***
 
 
-## Example 1: Exporting earnings for all hotspots in a given wallet
-# wallet that the hotspots are owned by
-ACCOUNT_ADDRESS = '14b7gkGPca2zyRCbUr1uuykiJwdtYnDhdZ3XBKgXocKQANrSavd'
-WALLET_EXPORT_PATH = 'account_earnings_2020.csv'
+# Example 1: Exporting earnings for all hotspots in a given wallet
+ACCOUNT_ADDRESS = 'ae9iurqijoaFAKEADDRESSsdfiojewroijssn'  # wallet that the hotspots are owned by
+WALLET_EXPORT_PATH = 'account_earnings_2021.csv'
 
 wallet_earnings_df, total_wallet_earnings = export_wallet_taxes(ACCOUNT_ADDRESS)
 # export a CSV
 wallet_earnings_df.to_csv(WALLET_EXPORT_PATH)
 
-## Example 2: Exporting earnings for a single hotspot
+# Example 2: Exporting earnings for all hotspots individually in a given wallet
+ACCOUNT_ADDRESS = 'ae9iurqijoaFAKEADDRESSsdfiojewroijssn'  # wallet that the hotspots are owned by
+# csvs export to the working directory
+wallet_earnings_dfs, total_wallet_earnings = export_wallet_taxes_by_hotspot(ACCOUNT_ADDRESS)
+
+# Example 3: Exporting earnings for a single hotspot
 # hotspot address
 HOTSPOT_ADDRESS = '112BTTz2TLqqmGiP7Zi2j1oyFyakjrJhumSoukEvQuzQ4ztQQVuB'
 
